5120f79076a5444ddca16c5f47d5fb53e31f4c5d
Neto <ernesto.rodriguez@qapio.com>
2021-09-16 15:51:21 -0600

    Tipos de datos y Ejercicios de naturales

---
+++ clases/16-09/Main.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Main where
+
+import Prelude (Read(..), Show(..), read, undefined)
+
+data Bool = True | False deriving (Read, Show)
+
+not :: Bool -> Bool
+not True = False
+not False = True
+
+False && _ = False
+_ && False = False
+_  && _ = True
+
+True || _ = True
+_ || True = True
+_ || _ = False
+
+ifThenElse True onTrue _ = onTrue
+ifThenElse False _ onFalse = onFalse
+
+data Natural = Cero | Succ Natural deriving Show
+
+Cero + a = a
+a + Cero = a
+a + (Succ b) = Succ (a + b)
+
+-- Ejercicio: Implementar la multiplicacion
+-- de numeros naturales.
+a * b = undefined
+
+Cero > _ = False
+_ > Cero = True
+Succ a > Succ b = a > b
+
+-- Ejercicio: Implementar el operador
+-- "menor que" para numeros naturales.
+a < b = undefined
+
+main = undefined

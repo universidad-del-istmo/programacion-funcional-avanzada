706ad6581ead139ed2a7b5452895dc848398e835
Ernesto Rodriguez <ernesto.rodriguez@qapio.com>
2021-09-17 14:50:45 -0600

    Foldl y foldr

--- clases/17-09/Main.hs
+++ clases/17-09/Main.hs
@@ -35,4 +35,41 @@ enumerar n = Cons n (enumerar (n - 1))
 range :: Int -> Int -> Lista
 range = undefined
 
+pushBack x Nil = Cons x Nil
+pushBack x (Cons a as) = Cons a (pushBack x as)
+
+reverse Nil = Nil
+reverse (Cons x xs) = pushBack x (reverse xs)
+
+-- Ejercicio: Definir la funcion "take"
+-- esta funcion toma una lista y un
+-- numero. Produce una nueva lista donde
+-- que tiene los primeros "n" numeros de
+-- la lista original.
+take :: Int -> Lista -> Lista
+take = undefined
+
+map :: (Int -> Int) -> Lista -> Lista
+map _ Nil = Nil
+map f (Cons x xs) = Cons (f x) (map f xs)
+
+foldl :: (estado -> Int -> estado) -> estado -> Lista -> estado
+foldl _ fNil Nil = fNil
+foldl fCons fNil (Cons x xs) =
+    fCons (foldl fCons fNil xs) x
+
+reverse' xs = foldl acc Nil xs
+    where
+        acc estado x = pushBack x estado
+
+foldr :: estado -> (Int -> estado -> estado) -> Lista -> estado
+foldr fNil fCons xs = aux fNil xs
+    where
+        aux estado (Cons x xs) = aux (fCons x estado) xs
+        aux estado Nil = estado
+
+reverse'' xs = foldr Nil acc xs
+    where
+        acc a as = Cons a as
+
 main = undefined
